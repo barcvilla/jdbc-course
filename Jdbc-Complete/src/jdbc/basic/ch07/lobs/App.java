8c3265be9691f982db57cbe85fc0da71123ae3ad
Carlos Villanueva <ceva_19@hotmail.com>
2019-08-20 22:00:24 -0300

    Java JDBC - LOBS

--- Jdbc-Complete/src/jdbc/basic/ch07/lobs/App.java
+++ Jdbc-Complete/src/jdbc/basic/ch07/lobs/App.java
@@ -5,12 +5,34 @@
  */
 package jdbc.basic.ch07.lobs;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner;
+
 /**
  *
  * @author PC
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        PersonService service = new PersonServiceImpl();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese nombre: ");
+        String name = input.next();
         
+        File file = new File(Ini.getPropertyFile1());
+        FileInputStream fis = new FileInputStream(file);
+        Persons p = new Persons();
+        p.setName(name);
+        p.setPic(fis);
+        int rc = service.insertPerson(p);
+        if(rc == 0)
+        {
+            System.out.println("record not inserted");
+        }
+        else
+        {
+            System.out.println("record inserted");
+        }
     }
 }
